551c521215a944fc9ffa6f025184c9ab6d193f01
habib <habib.khademhosseini@gmail.com>
2020-12-27 11:57:22 +0330

    add profile context

---
+++ src/context/profileContext.js
@@ -0,0 +1,58 @@
+import React, { createContext, useContext, useReducer } from 'react';
+
+const profileDispatchContext = createContext(null);
+const profileStateContext = createContext(null);
+
+const useProfileDispatchContext = () => {
+  const context = useContext(profileDispatchContext);
+  if (context === undefined) {
+    throw new Error('it should have a value');
+  }
+  return context;
+};
+
+const useProfileStateContext = () => {
+  const context = useContext(profileStateContext);
+  if (context === undefined) {
+    throw new Error('it should have a value');
+  }
+  return context;
+};
+
+const useProfile = () => {
+  return [useProfileStateContext(), useProfileDispatchContext];
+};
+
+const initialState = {
+  loading: true,
+  profile: {},
+};
+
+const ProfileReducer(state, action) => {
+    switch(action.type){
+        case 'get':
+            return {
+                ...state,
+                profile:action.payload
+            }
+        case 'loading':
+            return {
+                ...state,
+                loading: action.payload
+            }
+        default:
+            return state
+
+    }
+}
+
+const profileProvider=({children}) => {
+    const [state, dispatch] = useReducer(ProfileReducer,initialState);
+    return (
+        <profileDispatchContext.Provider value={dispatch}>
+            <profileStateContext.Provider value={state}>
+                {children}
+            </profileStateContext.Provider>
+        </profileDispatchContext.Provider>
+    )
+}
