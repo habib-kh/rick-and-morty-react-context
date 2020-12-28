b75dd3562cf977b739a3e85c94390143fa351bbf
habib <habib.khademhosseini@gmail.com>
2020-12-28 12:11:23 +0330

    add context to profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,46 +1,34 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import _ from 'underscore';
-import {
-  getEpisodes,
-  getLocation,
-  getOrigin,
-  getProfile,
-} from '../store/actions';
+
 import { Layout, CharachterProfile } from '../components';
+import { getProfile, useProfile } from '../context/profileContext';
+import { getEpisodes, useEpisode } from '../context/episodeContext';
+import { useLocation } from 'react-router-dom';
+import { getLocation } from '../store/actions';
 
 export default function Profile({
   match: {
     params: { id },
   },
 }) {
-  const loading = useSelector((state) => state.profile.loading);
-  const episodesLoading = useSelector((state) => state.episodes.loading);
-  const originLoading = useSelector((state) => state.origin.loading);
-  const locationLoading = useSelector((state) => state.location.loading);
-
-  const profile = useSelector((state) => state.profile.profile);
-  const episodes = useSelector((state) => state.episodes.episodes);
-  const location = useSelector((state) => state.location.location);
-  const origin = useSelector((state) => state.origin.origin);
-
-  const dispatch = useDispatch();
+  const [profileState, profileDispatch] = useProfile();
+  const { profile, loading: profileLoading } = profileState;
+  const [episodeState, episodeDispatch] = useEpisode();
+  const { episodes, loading: episodeLoading } = episodeState;
+  const [locationState, locationDispatch] = useLocation();
+  const { location, loading: locationLoading } = locationState;
 
   useEffect(() => {
-    dispatch(getProfile(id));
+    getProfile(profileDispatch, id);
   }, []);
 
   //get location and origin of profile
   useEffect(() => {
     if (!_.isEmpty(profile)) {
-      dispatch(getEpisodes(profile.episode));
-
+      getEpisodes(episodeDispatch, profile.episode);
       if (profile.location.url) {
-        dispatch(getLocation(profile.location.url));
-      }
-
-      if (profile.origin.url) {
-        dispatch(getOrigin(profile.origin.url));
+        getLocation(locationDispatch, profile.location.url);
       }
     }
   }, [profile]);
@@ -50,13 +38,12 @@ export default function Profile({
       page='profile-page'
       title={profile?.name}
       desc={profile?.location?.name}
-      loading={loading || originLoading || locationLoading || episodesLoading}
+      loading={profileLoading || locationLoading || episodeLoading}
     >
       <CharachterProfile
         profile={profile}
         episodes={episodes}
         location={location}
-        origin={origin}
       />
     </Layout>
   );
