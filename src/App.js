0d37f61723c242032f9f63272f7bb5ec1855bda2
habib <habib.khademhosseini@gmail.com>
2020-12-28 12:10:44 +0330

    add context to routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Provider } from 'react-redux';
 import store from './store/store';
 import { Home, Profile } from './pages';
 import { CharacterProvider } from './context/character-context';
+import { ProfileProvider } from './context/profileContext';
+import { EpisodeProvider } from './context/episodeContext';
+import { LocationProvider } from './context/locationContext';
 
 export default function App() {
   return (
@@ -10,11 +13,31 @@ export default function App() {
     <Provider store={store}>
       <BrowserRouter>
         <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/profile/:id' exact component={Profile} />
+          <Route path='/' exact component={HomeComponent} />
+          <Route path='/profile/:id' exact component={ProfileComponent} />
         </Switch>
       </BrowserRouter>
       {/* // </Provider> */}
     </Provider>
   );
 }
+
+const HomeComponent = () => {
+  return (
+    <CharacterProvider>
+      <Home />
+    </CharacterProvider>
+  );
+};
+
+const ProfileComponent = () => {
+  return (
+    <ProfileProvider>
+      <EpisodeProvider>
+        <LocationProvider>
+          <Profile />
+        </LocationProvider>
+      </EpisodeProvider>
+    </ProfileProvider>
+  );
+};
